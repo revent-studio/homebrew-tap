b6422c63f57cb5f54f0cb8a90ee8110a9b385301
Maksym <maks@revent.studio>
2023-05-24 01:36:22 -0500

    gfb v1.1.2

--- Formula/gfb.rb
+++ Formula/gfb.rb
@@ -1,9 +1,9 @@
 class Gfb < Formula
     desc "Tiny CLI for enforcing branching name strategy across multiple git repositories"
     homepage "https://github.com/byte-partisan/git-favorite-branch"
-    url "https://github.com/byte-partisan/git-favorite-branch/releases/download/release%2Fv1.1.1/gfb-v1.1.1-x86_64-apple-darwin.tar.gz"
-    sha256 "28b9b80bf9aee5983f7c636da93a4e67d2c7af1bc9f51d86161d42149ab481e0"
-    version "1.1.1"
+    url "https://github.com/byte-partisan/git-favorite-branch/releases/download/release%2Fv1.1.2/gfb-v1.1.2-x86_64-apple-darwin.tar.gz"
+    sha256 "90694929fdfc38873cf277377cf29999d0c8b8170c1c3317185d7d53a4d6e114"
+    version "1.1.2"
 
     def install
         bin.install "gfb"
