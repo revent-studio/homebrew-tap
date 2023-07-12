79edc9daedf931bbe1b46ad406ab321b9c2a73ca
Maksym <maks@revent.studio>
2023-07-11 22:03:32 -0500

    Update gfb sha

--- Formula/gfb.rb
+++ Formula/gfb.rb
@@ -2,7 +2,7 @@ class Gfb < Formula
     desc "Tiny CLI for enforcing branching name strategy across multiple git repositories"
     homepage "https://github.com/byte-partisan/git-favorite-branch"
     url "https://github.com/byte-partisan/git-favorite-branch/releases/download/release%2Fv1.1.3/gfb-v1.1.3-x86_64-apple-darwin.tar.gz"
-    sha256 "90694929fdfc38873cf277377cf29999d0c8b8170c1c3317185d7d53a4d6e114"
+    sha256 "0e4d9c747721b29638499a535e1f7770233b0e0386b1f7db6ea2bbb7b56a4bf4"
     version "1.1.3"
 
     def install
